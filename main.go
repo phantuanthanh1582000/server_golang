209a5c005cc2398022f12ab9eba8d456c2891f1d
WINDOWS-PC\Windows <tphan10932@gmail.com>
2024-12-01 13:54:57 +0700

    server_golang

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("api/v1/list-product", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "List product",
+		})
+	})
+	r.Run(":3000")
+}
